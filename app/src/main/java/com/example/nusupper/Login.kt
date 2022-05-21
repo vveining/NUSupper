0a048d79aca0a9beef0398d8ede14ec027cd9559
elysia <elysiaojr@gmail.com>
2022-05-21 22:43:53 +0800

    changed profile page into an activity

--- app/src/main/java/com/example/nusupper/Login.kt
+++ app/src/main/java/com/example/nusupper/Login.kt
@@ -1,11 +1,19 @@
 package com.example.nusupper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.button)
+        loginButton.setOnClickListener {
+            val toLogin = Intent(this, Profile::class.java)
+            startActivity(toLogin)
+        }
     }
 }
