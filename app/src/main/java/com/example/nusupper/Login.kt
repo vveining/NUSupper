dcb32ade4983cdfd71ca3a7e94ed24566fab63b9
elysia <elysiaojr@gmail.com>
2022-05-24 18:17:04 +0800

    firebase authentication and database

--- app/src/main/java/com/example/nusupper/Login.kt
+++ app/src/main/java/com/example/nusupper/Login.kt
@@ -3,17 +3,45 @@ package com.example.nusupper
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
+import com.example.nusupper.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
 
-        val loginButton = findViewById<Button>(R.id.button)
-        loginButton.setOnClickListener {
-            val toLogin = Intent(this, Profile::class.java)
-            startActivity(toLogin)
+        binding.button.setOnClickListener {
+            if (checkFields()) {
+                val email = binding.EmailAddress.text.toString()
+                val password = binding.Password.text.toString()
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, Profile::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(this, "invalid email and/or password!",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "empty fields are not allowed",
+                    Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun checkFields(): Boolean {
+        return (binding.EmailAddress.text.toString().trim{it<=' '}.isNotEmpty()
+                && binding.Password.text.toString().trim{it<=' '}.isNotEmpty())
     }
 }
