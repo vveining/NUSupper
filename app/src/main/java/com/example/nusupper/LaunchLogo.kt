50469466d71bbd2e4e69c3e4eddd829e973ab6d9
wei ning yong <yongweining.ywn@gmail.com>
2022-06-07 17:12:12 +0800

    added launcher page and stored login checks in LaunchLogo.kt

---
+++ app/src/main/java/com/example/nusupper/LaunchLogo.kt
@@ -0,0 +1,35 @@
+package com.example.nusupper
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import com.google.firebase.auth.FirebaseAuth
+//import com.google.firebase.auth.FirebaseUser
+
+class LaunchLogo : AppCompatActivity() {
+    private lateinit var launchAuth:FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch_logo)
+
+        launchAuth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //val user: FirebaseUser? = launchAuth.currentUser
+        // ^ this statement should be used! set user to null so that users
+        // have to login once they quit the app
+        val user = null
+        Toast.makeText(this,user.toString(),LENGTH_SHORT).show()
+        if (user == null) {
+            startActivity(Intent(this,MainActivity::class.java))
+        } else {
+            startActivity(Intent(this,FindJio::class.java))
+        }
+    }
+}
+
