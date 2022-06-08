0a9a47d593774755ac4cfeb33b14af4606bc8e14
wei ning yong <yongweining.ywn@gmail.com>
2022-06-08 12:19:35 +0800

    added onclick listener for "your current jios"

--- app/src/main/java/com/example/nusupper/JiosAdapter.kt
+++ app/src/main/java/com/example/nusupper/JiosAdapter.kt
@@ -9,11 +9,23 @@ import com.example.nusupper.models.Jio
 import kotlinx.android.synthetic.main.item_jio.view.*
 
 class JiosAdapter (val context: Context, val jios: List<Jio>) :
+
     RecyclerView.Adapter<JiosAdapter.ViewHolder>() {
+
+    private lateinit var jListener : onItemClickListener
+
+    interface onItemClickListener{
+        fun onItemClick(position: Int)
+
+    }
+
+    fun setItemClickListener(listener: onItemClickListener) {
+        jListener = listener
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_jio, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(view, jListener)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -22,13 +34,23 @@ class JiosAdapter (val context: Context, val jios: List<Jio>) :
 
     override fun getItemCount() = jios.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View, listener: onItemClickListener) : RecyclerView.ViewHolder(itemView) {
         fun bind(jio: Jio) {
 
             itemView.textView.text = jio.restaurant
             itemView.textView2.text = jio.location
             itemView.textView3.text = jio.closeTime.toString()
             itemView.textView4.text = jio.creator?.username
+            //add image for restaurant here
+        }
+
+        init {
+            itemView.setOnClickListener{
+                listener.onItemClick(adapterPosition)
+            }
         }
     }
+
+    //onclick listener for recycler view
+
 }
