ff54eeb3165bcf332a08ede953861b06d53dac74
wei ning yong <yongweining.ywn@gmail.com>
2022-06-10 14:04:11 +0800

    first half of view jio page frontend

--- app/src/main/java/com/example/nusupper/ViewJio.kt
+++ app/src/main/java/com/example/nusupper/ViewJio.kt
@@ -1,11 +1,72 @@
 package com.example.nusupper
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.nusupper.databinding.ActivityTemplateBinding
+import com.google.android.material.navigation.NavigationView
 
 class ViewJio : AppCompatActivity() {
+
+    private lateinit var mDrawerLayout: DrawerLayout
+    private lateinit var binding: ActivityTemplateBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_jio)
+        //setContentView(R.layout.activity_template)
+        binding = ActivityTemplateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar: Toolbar = findViewById(R.id.template_toolbar)
+        setSupportActionBar(toolbar)
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+        }
+
+        mDrawerLayout = findViewById(R.id.template_drawer_layout)
+
+        val navigationView: NavigationView = findViewById(R.id.template_nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            // handle navigation view item clicks here
+            when (menuItem.itemId) {
+
+                R.id.profile -> {
+                    Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show()
+                }
+                R.id.createjio -> {
+                    Toast.makeText(this, "create a Jio", Toast.LENGTH_SHORT).show()
+                }
+                R.id.findjio -> {
+                    Toast.makeText(this, "find a Jio", Toast.LENGTH_SHORT).show()
+                }
+                R.id.orderhistory -> {
+                    Toast.makeText(this, "order history", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+    }
+
+    //appbar - toolbar button click
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
