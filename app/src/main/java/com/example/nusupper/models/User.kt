580c25f3f4574960e9d434eda45b141e1870c547
wei ning yong <yongweining.ywn@gmail.com>
2022-06-26 11:59:31 +0800

    added residences displayed on profile

--- app/src/main/java/com/example/nusupper/models/User.kt
+++ app/src/main/java/com/example/nusupper/models/User.kt
@@ -1,5 +1,6 @@
 package com.example.nusupper.models
 
+import com.example.nusupper.R
 import com.google.firebase.firestore.PropertyName
 
 data class User(
@@ -11,4 +12,25 @@ data class User(
     var username: String = "",
     var paylah: Boolean = false,
     var paynow: Boolean = false,
-    var grabpay: Boolean = false)
+    var grabpay: Boolean = false) {
+
+    companion object {
+        fun getResImg(residence: String): Int {
+            when (residence) {
+                "CAPT" -> return R.drawable.capt
+                "RC4" -> return R.drawable.rc4
+                "Tembusu" -> return R.drawable.tembusu
+                "RVRC" -> return R.drawable.rvrc
+                "Cinnamon" -> return R.drawable.cinnamon
+                "Kent Ridge Hall" -> return R.drawable.kentridge
+                "Sheares Hall" -> return R.drawable.sheares
+                "Eusoff Hall" -> return R.drawable.eusoff
+                "Temasek Hall" -> return R.drawable.temasek
+                "Raffles Hall" -> return R.drawable.raffles
+                "King Edward VII Hall" -> return R.drawable.ke7
+                "PGPR" -> return R.drawable.pgpr
+            }
+            return R.drawable.capt
+        }
+    }
+}
